8f83a9bf54ae203b5a63cca3cc9ccaa1b23363fa
Ryan Bagwell <ryan@ryanbagwell.com>
2021-07-12 11:08:41 -0400

    data is only being updated monday through friday, so retry by going back a day until we get a succcess

--- downloadDataFile.js
+++ downloadDataFile.js
@@ -3,34 +3,62 @@ const fs = require(`fs`)
 const path = require(`path`)
 const axios = require("axios")
 const format = require('date-fns/format')
+const sub = require('date-fns/sub');
 
-let url = process.argv[2]
+let suppliedUrl = process.argv[2];
 
-if (!url) {
 
-  const month = format(new Date(), "MMMM").toLowerCase();
-  const day = format(new Date(), 'd')
-  const year = format(new Date(), 'yyyy')
+const getDownloadUrl = (offset = 0) => {
+
+  if (suppliedUrl) return suppliedUrl;
+
+  const offsetDate = sub(new Date(), {days: offset})
+
+  const month = format(offsetDate, "MMMM").toLowerCase();
+  const day = format(offsetDate, 'd')
+  const year = format(offsetDate, 'yyyy')
 
   url = `https://www.mass.gov/doc/covid-19-raw-data-${month}-${day}-${year}/download`
 
-  process.stdout.write(
-    `No url supplied. Trying to download from ${url}\n`
-  )
+  return url;
 
 }
 
-;(async function runIt() {
-  const response = await axios.get(url, {
-    responseType: "arraybuffer",
-  })
+; (async function runIt() {
 
-  fs.writeFileSync(
-    path.join(__dirname, "src", "data", `latest.xlsx`),
-    response.data,
-    "binary"
-  )
+  let offset = 0;
 
-  process.stdout.write("Done!\n")
-  process.exit(0)
+  const saveIt = (data) => {
+    fs.writeFileSync(
+      path.join(__dirname, "src", "data", `latest.xlsx`),
+      data,
+      "binary"
+    )
+  }
+
+  const getIt = async (url) => {
+
+    process.stdout.write(
+      `Trying to download from ${url}\n`
+    )
+
+    return await axios.get((url), {
+      responseType: "arraybuffer",
+    }).catch(err => { throw err });
+  }
+
+  const doItUntilSuccess = async () => {
+    try {
+      const response = await getIt(getDownloadUrl(offset));
+      saveIt(response.data);
+      process.stdout.write("Done!\n")
+      process.exit(0)
+    } catch (err) {
+      offset++;
+      process.stdout.write('Error. Going back a day ...\n');
+      doItUntilSuccess();
+    }
+  }
+
+  doItUntilSuccess()
 })()
